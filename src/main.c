5f11af884148173b75b046b42bd637a9b039c8ec
slopey <howard_peng@icloud.com>
2020-12-02 22:53:41 -0800

    add barycentric algo

--- src/main.c
+++ src/main.c
@@ -1,20 +1,26 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <assert.h>
+#include <math.h>
 
 #include "vector.h"
 #include "types.h"
 #include "canvas.h"
 
+#define EPSILON 0.00001
+
 int main()
 {
+    // Dot Product Test
     Vector *a = vector_init(-6, 8, 0);
     Vector *b = vector_init(5, 12, 0);
     double dot = vector_dot(a, b);
-    printf("%f\n", dot);
+    assert(dot == 66);
     free(a);
     free(b);
 
+    // Ray-plane Intersection Test
     Vector *normal = vector_init(0, 0, 1);
     Vector *center = vector_init(0, 0, 5);
     Plane *plane = plane_init(normal, center);
@@ -23,22 +29,43 @@ int main()
     Vector *v = vector_init(0, -1, -1);
 
     Vector *intersect = plane_ray_intersect(plane, origin, v);
-    free(normal);
-    free(center);
-    free(plane);
+    plane_destroy(plane);
     free(origin);
     free(v);
 
-    printf("%f %f %f\n", intersect->x, intersect->y, intersect->z);
+    assert(fabs(intersect->x - 0) < EPSILON);
+    assert(fabs(intersect->y + 5) < EPSILON);
+    assert(fabs(intersect->z - 5) < EPSILON);
     free(intersect);
 
+    // Ray-triangle Intersection Test
+    Vector *p1 = vector_init(0, 0, 0);
+    Vector *p2 = vector_init(10, 0, 0);
+    Vector *p3 = vector_init(0, 10, 0);
+    Triangle *triangle = triangle_init(p1, p2, p3);
+
+    Vector *test1 = vector_init(50, 0, 0);
+    Vector *test2 = vector_init(5, 0, 0);
+
+    int val1 = triangle_intersect_point(triangle, test1);
+    int val2 = triangle_intersect_point(triangle, test2);
+
+    assert(val1 == 0);
+    assert(val2 == 1);
+
+    triangle_destroy(triangle);
+    free(test1);
+    free(test2);
+
+    // Canvas Display Test
+    // (you should see a thin red horizontal line on the top half of the window.
     Canvas *canvas = canvas_init(100, 100, 20, 20);
     for (int i = 0; i < 100; i++)
     {
         canvas_draw(canvas, i, 10, 255, 0, 0);
     }
     canvas_display(canvas);
-    sleep(5);
+    sleep(1);
     canvas_destroy(canvas);
     return 0;
 }
--- src/types.c
+++ src/types.c
@@ -24,6 +24,13 @@ Plane *plane_init(Vector *normal, Vector *center)
     return plane;
 }
 
+void plane_destroy(Plane *self)
+{
+    free(self->normal);
+    free(self->center);
+    free(self);
+}
+
 Triangle *triangle_init(Vector *p1, Vector *p2, Vector *p3)
 {
     Triangle *triangle = malloc(sizeof(Triangle));
@@ -31,6 +38,14 @@ Triangle *triangle_init(Vector *p1, Vector *p2, Vector *p3)
     triangle->p2 = p2;
     triangle->p3 = p3;
     return triangle;
+}
+
+void triangle_destroy(Triangle *self)
+{
+    free(self->p1);
+    free(self->p2);
+    free(self->p3);
+    free(self);
 }
 
 Plane *triangle_to_plane(Triangle *triangle)
@@ -43,4 +58,28 @@ Plane *triangle_to_plane(Triangle *triangle)
     
     Plane *plane = plane_init(cross, triangle->p1);
     return plane;
+}
+
+int triangle_intersect_point(Triangle *self, Vector *P)
+{
+    Vector *v0 = vector_minus(self->p2, self->p1);
+    Vector *v1 = vector_minus(self->p3, self->p1);
+    Vector *v2 = vector_minus(P, self->p1);
+
+    double d00 = vector_dot(v0, v0);
+    double d01 = vector_dot(v0, v1);
+    double d11 = vector_dot(v1, v1);
+    double d20 = vector_dot(v2, v0);
+    double d21 = vector_dot(v2, v1);
+    double d22 = vector_dot(v2, v2);
+    
+    double inverse = 1 / (d00 * d11 - d01 * d01);
+    double u = (d11 * d20 - d01 * d21) * inverse;
+    double v = (d00 * d21 - d01 * d20) * inverse;
+
+    free(v0);
+    free(v1);
+    free(v2);
+
+    return (u >= 0) && (v >= 0) && (u + v < 1);
 }
