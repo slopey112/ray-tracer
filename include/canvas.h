28ccae6205ba7730cff8f84a4bc8b8cf0606d826
slopey <howard_peng@icloud.com>
2020-12-02 22:54:30 -0800

    add header files

---
+++ include/canvas.h
@@ -0,0 +1,20 @@
+#include <SDL2/SDL.h>
+
+#include "types.h"
+#include "vector.h"
+
+typedef struct
+{
+    SDL_Window *window;
+    SDL_Renderer *renderer;
+    uint width;
+    uint height;
+    uint viewport_width;
+    uint viewport_height;
+} Canvas;
+
+Canvas *canvas_init(uint width, uint height, uint viewport_width, uint viewport_height);
+void *canvas_destroy(Canvas *self);
+void *canvas_draw(Canvas *self, uint x, uint y, byte r, byte g, byte b);
+void *canvas_display(Canvas *self);
+Vector *canvas_to_viewport(Canvas *self, uint canvas_x, uint canvas_y, uint distance);
--- include/types.h
+++ include/types.h
@@ -1,3 +1,6 @@
+#ifndef TYPES_H
+#define TYPES_H
+
 #include "vector.h"
 
 typedef struct
@@ -21,7 +24,15 @@ typedef struct
     Vector *p4;
 } Face;
 
+typedef unsigned int uint;
+typedef unsigned char byte;
+
 Triangle *triangle_init(Vector *p1, Vector *p2, Vector *p3);
 Plane *triangle_to_plane(Triangle *triangle);
 Plane *plane_init(Vector *normal, Vector *center);
 Vector *plane_ray_intersect(Plane *self, Vector *origin, Vector *v);
+int triangle_intersect_point(Triangle *self, Vector *P);
+void triangle_destroy(Triangle *self);
+void plane_destroy(Plane *self);
+
+#endif
